2b176e5b519527d0988ef8645bd4a1141efc6e2e
Ubin Jung <jungu@utexas.edu>
2021-08-16 16:27:54 -0700

    hooked up backend to frontend localhost

--- server/index.js
+++ server/index.js
@@ -7,21 +7,21 @@ const port = 8080
 
 // forward requests to API
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const API_KEY = require('./config/config.js');
-const apiUrl = 'http://localhost:3000';
+// const API_KEY = require('./config/config.js');
+const apiUrlReview = 'http://localhost:3000';
 
 // add authentication to request header
-const proxyOptions = {
-  target: apiUrl,
+const reviewProxyOptions = {
+  target: apiUrlReview,
   changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    proxyReq.setHeader('Authorization', API_KEY);
-  }
+  // onProxyReq: (proxyReq, req, res) => {
+  //   proxyReq.setHeader('Authorization', API_KEY);
+  // }
 }
 
 // establish middleware request forwarding
-app.use('/api', createProxyMiddleware(proxyOptions));
+app.use('/api', createProxyMiddleware(reviewProxyOptions));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
